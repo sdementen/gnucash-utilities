53759aa1ae9049a25cb30bab93b01c838b693a60
Sébastien de Menten <sdementen@gmail.com>
2016-11-13 10:31:11 +0100

    commit metadata file

---
+++ piecash_utilities/metadata.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""Project metadata
+
+Information describing the project.
+"""
+
+# The package name, which is also the "UNIX name" for the project.
+package = 'piecash_utilities'
+project = "Set of script leveraging piecash"
+project_no_spaces = project.replace(' ', '')
+version = '0.0.2'
+description = 'Set of script leveraging piecash.'
+authors = ['sdementen']
+authors_string = ', '.join(authors)
+emails = ['sdementen@gmail.com']
+license = 'MIT'
+copyright = '2015 ' + authors_string
+url = 'https://github.com/sdementen/gnucash-utilities'
