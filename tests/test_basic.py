a4592f63501a7cead988f84c01ec676767fd4eab
Sébastien de Menten <sdementen@gmail.com>
2016-11-13 17:16:42 +0100

    remove inexisting import

--- tests/test_basic.py
+++ tests/test_basic.py
@@ -3,8 +3,6 @@ import pytest
 
 import sys
 import codecs
-import piecash
-from test_helper import file_template_full
 
 if sys.version_info.major == 2:
     out = codecs.getwriter('UTF-8')(sys.stdout)
