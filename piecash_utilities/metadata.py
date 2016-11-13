b65148c5d6975309406b1849a71999f3eb5f4fee
Sébastien de Menten <sdementen@gmail.com>
2016-11-13 16:04:20 +0100

    dump version 0.0.3

--- piecash_utilities/metadata.py
+++ piecash_utilities/metadata.py
@@ -8,7 +8,7 @@ Information describing the project.
 package = 'piecash_utilities'
 project = "Set of script leveraging piecash"
 project_no_spaces = project.replace(' ', '')
-version = '0.0.2'
+version = '0.0.3'
 description = 'Set of script leveraging piecash.'
 authors = ['sdementen']
 authors_string = ', '.join(authors)
